87c0c0fbc95c0dd8d669cfe115ed91497b968197
Kenneth Ng <kenneth.ng.4@gmail.com>
2018-03-19 22:28:13 -0400

    edit callback to update friends? column after create if both both friends have sent friend requests

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -12,36 +12,35 @@
 
 class Friendship < ApplicationRecord
     validates :friend1_id, :friend2_id, :friends?, presence: true
+    validates_uniqueness_of :friend1_id, :scope => [:friend2_id]
     validates :friends?, inclusion: { in: [ true, false ] }
-    validates :friendship_exists?
+    validate :update_friendship?, after: :create
+    
 
     belongs_to(
         :friend1,
-        primary_key: :friend1_id,
+        primary_key: :id,
         foreign_key: :friend2_id,
         class_name: :User
     )
 
     belongs_to(
         :friend2,
-        primary_key: :friend1_id,
+        primary_key: :id,
         foreign_key: :friend2_id,
         class_name: :User
     )
 
     private
-    
-    def friendship_exists?
-        # example of lazy loading
+
+    def update_friendship?
         friendship_1 = Friendship.where('friend1_id = ? AND friend2_id', friend1.id, friend2.id)
         friendship_2 = Friendship.where('friend1_id = ? AND friend2_id', friend2.id, friend1.id)
         
-        if  friendship_1 || friendship_2
-            if friendship_1.first.friends? && friendship_1.first.friends?
-                return errors.add(:friendship, "You're already friends!")
-            else
-                return errors.add(:friendship, "You already sent a friend request!")
-            end
-        end 
+        if friendship_1 && friendship_2
+            friendship_1.first.friends? = true
+            friendship_2.first.friends? = true
+        end
     end
+
 end
